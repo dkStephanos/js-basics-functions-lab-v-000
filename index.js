1698e6c36478d1164dd3c04674ff6ad908fa2e76
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 05:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ function distanceTravelledInFeet(block1, block2) {
   return Math.abs(block1 - block2) * 264
 }
 
-function calculatesFarePrice() {
+function calculatesFarePrice(block1, block2) {
+  feet = distanceTravelledInFeet(block1, block2)
 
+  if (feet > 2500) {
+    return "cannot travel that far"
+  } else if (feet > 2000) {
+    return 25
+  } else {
+    feet -= 400
+    return feet * .02
+  }
 }
