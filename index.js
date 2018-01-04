270003a91390708cb1450efe6c93db4cc6ed5829
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 05:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function distanceFromHqInBlocks(block) {
     return 42 - block
   }
 }
+
+function distanceFromHqInFeet(block) {
+  const blocks = distanceFromHqInBlocks(block)
+
+  return blocks * 264
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs(block1 - block2) * 264
+}
+
+function calculatesFarePrice()
